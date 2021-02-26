ee39a9e143079c8399239aaae77bce7582b741ad
repl.it user <replituser@example.com>
2021-02-26 11:12:28 +0000

    Added messages files to output

---
+++ Basics/Output/multiline_message.py
@@ -0,0 +1,7 @@
+#Display message to standard output in multiple line of code
+print("System Failure Imminent!")
+print("System Reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hoover engine\n")
+print ("System online")
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print ("Hello")
+print ("First step: git add .")
+print ("Second step: git commit -m ]\"Your message here\"")
+print ("Last step: git push")
